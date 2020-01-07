2851f273cd9897789dbc6ca0e190bb4fcedf25de
Z0bby <59617066+Z0bby@users.noreply.github.com>
2020-01-07 15:29:56 -0500

    To be honest, im not 100% sure

--- Robot Code 2020/src/main/java/frc/robot/RobotContainer.java
+++ Robot Code 2020/src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.CommandLift;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.LiftSub;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -25,7 +27,9 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
+private final LiftSub m_liftSub = new LiftSub();
 
+private final CommandLift m_commandLift = new CommandLift(m_liftSub);
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
