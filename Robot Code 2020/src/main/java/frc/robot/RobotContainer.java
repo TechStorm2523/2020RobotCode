941c274d203a2348154a6226352500650a4258b6
TechStorm2523 <tech.storm@stjlabs.com>
2020-01-07 15:29:55 -0500

    Added command for launcher

--- Robot Code 2020/src/main/java/frc/robot/RobotContainer.java
+++ Robot Code 2020/src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.SpinLauncherRPM;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Launcher;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -22,9 +24,10 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Launcher m_launcher = new Launcher();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
-
+  private final SpinLauncherRPM m_spinLauncherRPM = new SpinLauncherRPM(m_launcher);
 
 
   /**
