b0379b4ba2d597e6ef9ab001de7207e0a6146a3d
ColinDavidBarney <59617016+ColinDavidBarney@users.noreply.github.com>
2020-01-08 17:24:24 -0500

    Added Manipulator Commands
    Learning to git, trial by fire, pushing children out of tree nests. HELP

---
+++ Robot Code 2020/src/main/java/frc/robot/subsystems/Manipulator.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Manipulator extends SubsystemBase {
+  /**
+   * Creates a new Manipulator.
+   */
+  WPI_TalonSRX Manipulator = new WPI_TalonSRX(4)
+  public Manipulator() {
+
+  }
+  //Constructor boi
+  public void SpinClockwise(){
+    Manipulator.set(1);
+    
+
+  }
+  public void SpinCounterClockwise(){
+    Manipulator.set(-1);
+    
+
+  }
+  public void Spin(double Speed){
+    Manipulator.set(Speed);
+    
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
--- Robot Code 2020/src/main/java/frc/robot/subsystems/PIDtest.java
+++ Robot Code 2020/src/main/java/frc/robot/subsystems/PIDtest.java
@@ -9,45 +9,30 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
-public class DriveTrain extends PIDSubsystem {
+public class PIDtest extends PIDSubsystem {
   /**
-   * Creates a new DriveTrain.
+   * Creates a new PIDtest.
    */
-//Motor Declarations Go Here 
-WPI_TalonFX FrontLeft = new WPI_TalonFX(0);
-WPI_TalonFX FrontRight = new WPI_TalonFX(1);
-WPI_TalonFX BackLeft = new WPI_TalonFX(2);
-WPI_TalonFX BackRight = new WPI_TalonFX(3);
-SpeedControllerGroup LeftSide = new SpeedControllerGroup(FrontLeft, BackLeft);
-SpeedControllerGroup RightSide = new SpeedControllerGroup(FrontRight, BackRight);
-  public DriveTrain() {
+  WPI_TalonFX FrontLeft = new WPI_TalonFX(0);
+  public PIDtest() {
     super(
         // The PIDController used by the subsystem
         new PIDController(0, 0, 0));
   }
-  public void methodname(){
-
-  }
-  public void Drive(double Left, double Right){
-    LeftSide.set(Left);
-    RightSide.set(Right); 
-
-
-  }
-
 
   @Override
   public void useOutput(double output, double setpoint) {
     // Use the output here
+    FrontLeft.set(output)
   }
 
   @Override
   public double getMeasurement() {
     // Return the process variable measurement here
-    return 0;
+    int position = FrontLeft.getSelectedSensorPosition()
+    return position;
   }
 }
