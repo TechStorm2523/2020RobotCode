c24257c99c204c747dc0e8fd1e9033ada89eeb95
Z0bby <59617066+Z0bby@users.noreply.github.com>
2020-01-17 18:05:58 -0500

    Lots of Stuff

--- Robot Code 2020/src/main/java/frc/robot/commands/CommandHood.java
+++ Robot Code 2020/src/main/java/frc/robot/commands/CommandHood.java
@@ -34,7 +34,7 @@ public class CommandHood extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_hoodPosition.
+    m_hoodPosition.HoodPositionStop();
   }
 
   // Returns true when the command should end.
