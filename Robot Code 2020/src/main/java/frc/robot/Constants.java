1f45422a84ed63e6db178921577e9de448b8fa81
Z0bby <59617066+Z0bby@users.noreply.github.com>
2020-01-18 17:57:03 -0500

    Began Work on Robot Map

--- Robot Code 2020/src/main/java/frc/robot/Constants.java
+++ Robot Code 2020/src/main/java/frc/robot/Constants.java
@@ -15,5 +15,14 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+
+public class RobotMap {}
+
+public static int leftmotorim_Port = 1;
+
+public static int _Port = 2;
+
+public static int _Port = 3;
+
 public final class Constants {
 }
